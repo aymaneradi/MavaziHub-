656192d863c01402b0238b5eac4e673563e40a32
Baurel Tanekam <baurel.tanekam@medien-systempartner.de>
2026-06-02 13:00:04 +0200

    Implementation of JWT

---
+++ backend/src/main/java/de/nordbyte/mavazihub/auth/security/jwt/JwtService.java
@@ -0,0 +1,59 @@
+package de.nordbyte.mavazihub.auth.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    private final JwtProperties jwtProperties;
+
+    public String generateAccessToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshTokenExpiration()))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public  String extractusername (String token){
+        return extractClaims(token)
+                .getSubject();
+    }
+
+    public boolean isTokenvalid(String token){
+        return extractClaims(token)
+                .getExpiration()
+                .after(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecretKey());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
